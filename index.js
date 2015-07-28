b4ac6ba113a74564d4e0e37c179147c6b3cf7261
sald19 <carl.sald19@gmail.com>
2015-07-28 16:45:43 -0600

    primer commit del paquete de handlebars

---
+++ index.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+    elixir = require('laravel-elixir'),
+    concat = require('gulp-concat'),
+    declare = require('gulp-declare'),
+    wrap = require('gulp-wrap'),
+    handlebars = require('gulp-handlebars');
+
+var task = elixir.Task;
+
+elixir.extend('handlebars', function (src, options) {
+
+    var config =  {
+        src: src || 'resources/assets/js/templates/*.hbs',
+        outputDir: options.outputDir || 'public/js/',
+        outputFile: options.outputFile || 'templates.js',
+        nameSpace: options.nameSpace || 'app.templates'
+    };
+
+    console.log(config);
+
+    new task('handlebars', function(){
+        return gulp.src(config.src)
+            .pipe(handlebars())
+            .pipe(wrap('Handlebars.template(<%= contents %>)'))
+            .pipe(declare({
+                namespace: config.nameSpace,
+                noRedeclare: true
+            }))
+            .pipe(concat(config.outputFile))
+            .pipe(gulp.dest(config.outputDir));
+    }).watch(config.src);
+});
